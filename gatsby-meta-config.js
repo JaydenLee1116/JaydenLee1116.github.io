4e2200a0a3254f908984a571248bcadbe5fdfab8
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-07-20 23:34:27 +0900

    about 수정, 첫 글 테스트

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -43,12 +43,30 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '2020.12 ~ 2021.10',
+        activity: '신사업 기획팀 근무',
+        links: {},
+      },
+      {
+        date: '2021.11 ~ 2022.06',
+        activity: 'CodeStates AI Bootcamp 수료',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: 'https://codestates.com/course/ai',
+        },
+      },
+      {
+        date: '2022.04 ~ ',
+        activity: '웹 개발(프론트엔드) 공부 시작',
+        links: {
+          github: 'https://github.com/JaydenLee1116',
+          post: 'https://jayden1116.tistory.com/',
+        },
+      },
+      {
+        date: '2022.08 ~ ',
+        activity: 'F-lab 프론트엔드 멘토링 참여',
+        links: {
+          post: 'https://f-lab.kr/',
         },
       },
     ],
@@ -72,15 +90,12 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+        title: '준비 중',
+        description: '준비 중',
+        techStack: ['HTML', 'CSS', 'Javascript'],
+        thumbnailUrl: 'blog.png', // assets 폴더에 png 파일 지정 ex) blog.png
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          github: 'https://github.com/JaydenLee1116',
         },
       },
     ],
